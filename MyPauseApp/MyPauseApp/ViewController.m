89ca81b500c6c897715e1b66f10482bf60894ca5
Rohan Aurora <rohanaurora@gmail.com>
2014-06-26 00:11:30 -0400

    KVO Code added

--- MyPauseApp/MyPauseApp/ViewController.m
+++ MyPauseApp/MyPauseApp/ViewController.m
@@ -20,8 +20,12 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(pauseTheGame) name:@"BackgroundMode" object:nil];
+
+    // KVO Test
+    [self addObserver:self forKeyPath:@"view.frame" options:0 context:nil];
     
 }
+
 
 -(void) pauseTheGame {
     
@@ -34,10 +38,9 @@
 }
 
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    
+    NSLog(@"Observing...");
 }
 
 @end
